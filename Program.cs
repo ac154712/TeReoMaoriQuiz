00d2305722c57ef43cf1d2ebc0749f2d8981de06
ac154712 <ac154712@MOE-10704447>
2023-03-14 13:34:54 +1300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace TeReoMaoriQuiz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bank ATM");
+            Console.Write("Please enter your language by choosing number;\n1.English\n2.Maori\n3.Samoan\n4.Exit\nType here: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num == 1)
+            {
+                Console.WriteLine("Hello!");
+            }
+            else if (num == 2)
+            {
+                Console.WriteLine("kia ora!");
+            }
+            else if (num == 3)
+            {
+                Console.WriteLine("talofa!");
+            }
+            else
+            {
+                Console.WriteLine("Goodbye!");
+            }
+        }
+    }
+}
