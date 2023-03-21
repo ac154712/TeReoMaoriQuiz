2cd876137f5ba61ed6ac7a82ab5aa970432e7b38
Ronald Arzadon <ac154712@avcol.school.nz>
2023-03-21 13:24:21 +1300

    putting while loop if user enters an option not part of the available options. If user does that, it will say its invalid and will prompt the user to enter an option again.

--- Program.cs
+++ Program.cs
@@ -1,28 +1,73 @@
-﻿namespace TeReoMaoriQuiz
+﻿using System.ComponentModel.Design;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices;
+using System.Threading.Channels;
+
+namespace TeReoMaoriQuiz
 {
     internal class Program
     {
-        static void Main(string[] args)
+        
+         static void Main(string[] args)
         {
-            Console.WriteLine("Bank ATM");
-            Console.Write("Please enter your language by choosing number;\n1.English\n2.Maori\n3.Samoan\n4.Exit\nType here: ");
-            int num = Convert.ToInt32(Console.ReadLine());
-            if (num == 1)
-            {
-                Console.WriteLine("Hello!");
-            }
-            else if (num == 2)
-            {
-                Console.WriteLine("kia ora!");
-            }
-            else if (num == 3)
-            {
-                Console.WriteLine("talofa!");
-            }
-            else
-            {
-                Console.WriteLine("Goodbye!");
+            play();
+            
+
+         static void play()
+            { // start of play method
+                Console.WriteLine("Te Reo Maori Quiz");
+                Console.Write("Welcome!, please enter your name to start:  ");
+                string name = Console.ReadLine();
+                Menu(name);
+            } // end of play method 
+
+         static void Menu(string name)
+            { // start of menu method, asking user the difficulty (easy, med, hard)
+                Console.WriteLine($"\nHello {name}!");
+                Console.Write("Choose a difficulty by choosing a number below;\n1.Easy\n2.Medium\n3.Hard\n4.Exit\nType here: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                
+                if (num == 1)
+                {
+                    Easy();
+                }
+                else if (num == 2)
+                {
+                    Medium();
+                }
+                else if (num == 3)
+                {
+                    Hard();
+                }
+                else if (num == 4)
+                {
+                    Console.WriteLine("Goodbye!");
+                }
+                else
+                {
+                    do
+                    {
+                        Console.Write("Please enter a valid option: ");
+                        num = Convert.ToInt32(Console.ReadLine());
+                    } while (num != 1 || num != 2 || num != 3 || num != 4);
+                } //end of menu method
             }
         }
+        
+        static void Easy()
+        { // start of easy section
+            Console.Clear();
+            Console.WriteLine("Welcome to the Beginner's Quiz!");
+        } // end of easy section
+        static void Medium()
+        { // start of medium section
+            Console.Clear();
+            Console.WriteLine("Welcome to the Intermediate/Standard Quiz!");
+        } // end of medium section
+        static void Hard()
+        { // start of hard section
+            Console.Clear();
+            Console.WriteLine("Welcome to the Expert's Quiz!");
+        } // end of hard section
     }
 }
